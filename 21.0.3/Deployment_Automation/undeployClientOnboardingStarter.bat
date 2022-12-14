768884103764f8a8fb9ae125b6356a7562435085
Olaf Hahnl <Olaf.Hahnl@de.ibm.com>
2022-12-14 22:09:59 +0100

    Adding Windows batch files for 21.0.3 deployments

---
+++ 21.0.3/Deployment_Automation/undeployClientOnboardingStarter.bat
@@ -0,0 +1,5 @@
+@echo off
+echo.
+rem This file is to be used with CP4BA 21.0.3 starter deployment to remove the Client Onboarding scenario and associated labs
+rem Replace all placeholders that are represented with <text> labels before executing this file
+java -jar <TIMESTAMP>_DeploymentAutomation.jar -bootstrapURL=https://api.github.com/repos/IBM/cp4ba-client-onboarding-scenario/contents/21.0.3/Deployment_Automation/Starter -ocpAdminURL=<ocpAdminURL> -ocpAdminToken=<ocpAdminToken> -installBasePath=/Starter -config=config-undeploy -automationScript=RemoveClientOnboardingArtifacts.json
