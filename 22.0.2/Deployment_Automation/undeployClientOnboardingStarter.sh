766da593e9141e69f94cc320635f207da09b1a4a
Olaf Hahnl <Olaf.Hahnl@de.ibm.com>
2023-04-26 07:18:17 +0200

    Removed extra character

--- 22.0.2/Deployment_Automation/undeployClientOnboardingStarter.sh
+++ 22.0.2/Deployment_Automation/undeployClientOnboardingStarter.sh
@@ -167,4 +167,4 @@ then
   exit 1
 fi
 
-java -jar ${TOOLFILENAME} ${bootstrapDebugString} ${BOOTSTRAPURL} -ocLoginServer=${ocLoginServer} -ocLoginToken=${ocLoginToken} ${TOOLPROXYSETTINGS} -installBasePath=${DEPLOYMENTPATTERN} -config=c${CONFIGNAME} -automationScript=${AUTOMATIONSCRIPT}
+java -jar ${TOOLFILENAME} ${bootstrapDebugString} ${BOOTSTRAPURL} -ocLoginServer=${ocLoginServer} -ocLoginToken=${ocLoginToken} ${TOOLPROXYSETTINGS} -installBasePath=${DEPLOYMENTPATTERN} -config=${CONFIGNAME} -automationScript=${AUTOMATIONSCRIPT}
