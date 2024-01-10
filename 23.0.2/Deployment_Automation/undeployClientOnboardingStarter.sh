2668ab7e8fefd4922785059fa253163bee0d4eb3
OlafHahnl <Olaf.Hahnl@de.ibm.com>
2024-01-10 21:23:08 +0100

    Initial commit for CP4BA 23.0.2 (untested)

---
+++ 23.0.2/Deployment_Automation/undeployClientOnboardingStarter.sh
@@ -0,0 +1,229 @@
+#!/bin/bash
+###############################################################################
+#
+# Licensed Materials - Property of IBM
+#
+# (C) Copyright IBM Corp. 2023. All Rights Reserved.
+#
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+###############################################################################
+
+# This file is to be used with CP4BA 23.0.1 Starter deployment to remove the Client Onboarding scenario and associated labs
+
+# Set all variables according to your environment before executing this file
+
+#----------------------------------------------------------------------------------------------------------
+# Specify below variables to launch the deployment automation with
+#----------------------------------------------------------------------------------------------------------
+
+# Either 'pakInstallerPortalURL' or 'ocLoginServer' and 'ocLoginToken' need to be specified but not both depending on how environment is installed
+
+# URL of the PAK INSTALLER PORTAL directly from the 'Your environment is ready' email 'PakInstaller Portal URL:' when deployed from TechZone via Pak Installer
+pakInstallerPortalURL=REQUIRED
+
+# Value of the 'server' parameter as shown on the 'Copy login command' page in the OCP web console
+#ocLoginServer=REQUIRED
+# Value shown under 'Your API token is' or as 'token' parameter as shown on the 'Copy login command' page in the OCP web console
+#ocLoginToken=REQUIRED
+
+# Modify below three properties according to your needs. Only if the previous option is set to true, the next option can be set to true, too 
+# (e.g. only when cleanupClientOnboardingLabs_UserData is set to true, cleanupClientOnboardingLabs can be set to true)
+# Potential use-cases:
+# - You ran a workshop and want to reuse the environment, you may want to only remove user-data but keep the lab and scenario artifacts deployed (change the second and third option to false)
+# - You want to remove the whole Client Onboarding Artifacts including! custom artifacts created by business users leave all three properties set to true
+# Warning: cleanupClientOnboardingLabs_UserData does not remove data in CPE that was created using the admin user of the environment, only data created by the business users. If the admin user
+# created data undeploying the content lab artfiacts may fail and manual cleanup may be required of the data created by the admin user before content lab artifcats can be undeployed successfully
+cleanupClientOnboardingLabs_UserData=true
+cleanupClientOnboardingLabs=true
+cleanupClientOnboardingScenario=true
+
+# Uncomment in case GitHub is not accessible and all resources are already available locally
+#disableAccessToGitHub="-disableAccessToGitHub=true"
+
+# Proxy settings in case a proxy server needs to be used to access the GitHub resources
+# Uncomment at least the proxScenatio, proxyHost, and proxyPort lines and set values accordingly in case a proxy server needs to be used to access GitHub
+# Uncomment the lines proxyUser and proxyPwd too, in case the proxy server requires authentication
+#proxyScenario=GitHub
+#proxyHost=
+#proxyPort=
+#proxyUser=
+#proxyPwd=
+
+# Specific trace string for the boostrapping process (only uncomment if instructed to do so)
+#bootstrapDebugString="\"-bootstrapDebugString=*=finest\""
+
+# ----------------------------------------------------------------------------------------------------------
+# Construct the proxy settings for curl and deployment automation tool if proxy is configured
+# ----------------------------------------------------------------------------------------------------------
+
+if [ ! -z "${proxyHost+x}" ]
+then
+  if [ ! -z "${proxyUser+x}" ]
+  then
+     TOOLPROXYSETTINGS=-"proxyScenario=${proxyScenario} -proxyHost=${proxyHost} -proxyPort=${proxyPort} -proxyUser=${proxyUser} -proxyPwd=${proxyPwd}"
+
+     if [[ "${proxyScenario}" == "GitHub" ]] 
+     then
+       CURLPROXYSETTINGS="-x ${proxyHost}:${proxyPort} -U ${proxyUser}:${proxyPwd}"
+     fi
+  else
+    TOOLPROXYSETTINGS="-proxyScenario=${proxyScenario} -proxyHost=${proxyHost} -proxyPort=${proxyPort}"
+    if [[ "${proxyScenario}" == "GitHub" ]] 
+    then
+       CURLPROXYSETTINGS="-x ${proxyHost}:${proxyPort}"
+    fi
+  fi
+fi
+
+# ----------------------------------------------------------------------------------------------------------
+# Global settings for the script (not to be modified by user)
+# ----------------------------------------------------------------------------------------------------------
+
+# Source URL where the deployment automation jar can be retrieved from
+TOOLSOURCE="https://api.github.com/repos/IBM/cp4ba-client-onboarding-scenario/contents/Deployment_Automation/Current"
+# CP4BA version
+CP4BAVERSION="23.0.1"
+# Deployment pattern of the CP4BA instance
+DEPLOYMENTPATTERN="Starter"
+# Source URL to bootstrap configuration for the deployment tool
+BOOTSTRAPURL="-bootstrapURL=https://api.github.com/repos/IBM/cp4ba-client-onboarding-scenario/contents/${CP4BAVERSION}/Deployment_Automation/${DEPLOYMENTPATTERN}"
+# Name of the configuration file to use when running the deployment automation tool
+CONFIGNAME="config-undeploy"
+# Automation script to use when running the deployment automation tool
+AUTOMATIONSCRIPT="RemoveClientOnboardingArtifactsEmbeddedGitea.json"
+
+# Name of the source sh file passed to execution environment
+SCRIPTNAME=undeployClientOnboardingStarter.sh
+# Name of the actual sh file passed to execution environment
+FILENAME=$0
+# Version of this script file passed to execution environment
+SCRIPTVERSION=1.1.2
+# Download URL for this script
+SCRIPTDOWNLOADPATH=https://raw.githubusercontent.com/IBM/cp4ba-client-onboarding-scenario/main/${CP4BAVERSION%}/Deployment_Automation/${SCRIPTNAME%}
+
+# ----------------------------------------------------------------------------------------------------------
+# Retrieve the deployment automation jar file from GitHub if not already available or use local one when 
+# GitHub access is disabled. Validate that the deployment tool jar is available
+# ----------------------------------------------------------------------------------------------------------
+
+toolValidationSuccess=true
+echo "Bootstrapping deployment automation tool:"
+
+# In case access to GitHub is disabled check if the deployment automation jar is available locally
+if [ ! -z "${disableAccessToGitHub+x}" ]
+then
+  # 
+  if ls *DeploymentAutomation.jar 1> /dev/null 2>&1; then
+    # get the latest version of the file
+    TOOLFILENAME=$(ls *DeploymentAutomation.jar | tail -n 1)
+    
+    echo "  Using local deployment automation jar file '${TOOLFILENAME}' as retrieval from GitHub is disabled"
+  # if no file with the name found 
+  else
+    toolValidationSuccess=false
+    echo "  No version of the deployment automation jar could be found while retrieval from GitHub is disabled"
+  fi
+else
+  # Retrieve the download URL of the only deployment automation jar that is available in the GitHub repository
+  GITHUBENTRIES=$(curl -s -X GET ${TOOLSOURCE})
+
+  # Extract the download URL and the actual name of the deployment automation jar that is started later on
+  DOWNLOADURL="download_url\": \""
+  DOWNLOADLINE=${GITHUBENTRIES#*$DOWNLOADURL}
+  DOWNLOADURL="${DOWNLOADLINE%%\"*}"
+  TOOLFILENAME="${DOWNLOADURL##*/}"
+  
+  # In case the deployment automation jar does not yet exist locally download it otherwise print a msg that no download is required
+  if [[ ! -f ${TOOLFILENAME} ]] 
+  then
+      echo "  Downloading deployment automation jar ${TOOLFILENAME} from GitHub"
+      curl -O ${DOWNLOADURL} ${CURLPROXYSETTINGS}
+  else
+    echo "  Deployment automation jar file '${TOOLFILENAME}' already exists, no need to download it from Github"
+  fi
+  
+  # In case the tool still does not exist something went wrong during download
+  if [[ ! -f ${TOOLFILENAME} ]] 
+  then
+    toolValidationSuccess=false
+    echo "  Failure to download ${TOOLFILENAME} from ${DOWNLOADURL}"  
+  fi
+fi
+
+echo
+
+# ----------------------------------------------------------------------------------------------------------
+# Validation that all required parameters are set
+# ----------------------------------------------------------------------------------------------------------
+
+validationSuccess=true
+
+# if 'pakInstallerPortalURL' is not defined or set as default or empty then check that 'ocLoginServer' and 'ocLoginToken' are properly defined
+if [ -z "${pakInstallerPortalURL+x}" ] || [[ "${pakInstallerPortalURL}" == "REQUIRED" ]] || [[ "${pakInstallerPortalURL}" == "" ]]
+then
+	if [ -z "${ocLoginServer+x}" ] || [[ "${ocLoginServer}" == "REQUIRED" ]] || [[ "${ocLoginServer}" == "" ]]
+	then
+		if $validationSuccess
+		then
+			echo "Validating configuration failed:"
+			validationSuccess=false
+		fi
+		echo "  Variable 'ocLoginServer' has not been defined/set (nor is variable 'pakInstallerPortalURL' defined/set)"
+	else
+		INTERNALOCLOGINSERVER=-ocLoginServer=${ocLoginServer}
+	fi
+	
+	if [ -z "${ocLoginToken+x}" ] || [[ "${ocLoginToken}" == "REQUIRED" ]] || [[ "${ocLoginToken}" == "" ]]
+	then
+		if $validationSuccess
+		then
+			echo "Validating configuration failed:"
+			validationSuccess=false
+		fi
+		echo "  Variable 'ocLoginToken' has not been defined/set (nor is variable 'pakInstallerPortalURL' defined/set)"
+	else
+		INTERNALOCLOGINTOKEN=-ocLoginToken=${ocLoginToken}
+	fi
+else 
+	if ([[ "${ocLoginServer}" != "REQUIRED" ]] && [[ "${ocLoginServer}" != "" ]]) || ([[ "${ocLoginToken}" != "REQUIRED" ]] && [[ "${ocLoginToken}" != "" ]])
+	then
+		if $validationSuccess
+		then
+			echo "Validating configuration failed:"
+			validationSuccess=false
+		fi
+		echo "  Either 'ocLoginServer' and 'ocLoginToken' or 'pakInstallerPortalURL' can be specified but NOT both"
+	else
+		INTERNALPAKINSTALLERPORTALURL=-pakInstallerPortalURL=${pakInstallerPortalURL}
+	fi
+fi
+
+if ! $validationSuccess
+then
+  echo
+fi
+
+if ! $validationSuccess || ! $toolValidationSuccess
+then
+  echo "Exiting..."
+  exit 1
+fi
+
+if [ ! -z "${cleanupClientOnboardingLabs_UserData+x}" ]
+then
+	cleanupClientOnboardingLabs_UserDataInternal=enableCleanupSWATLabs_UserData=${cleanupClientOnboardingLabs_UserData}
+fi
+
+if [ ! -z "${cleanupClientOnboardingLabs+x}" ]
+then
+	cleanupClientOnboardingLabsInternal=enableCleanupSWATLabs=${cleanupClientOnboardingLabs}
+fi
+
+if [ ! -z "${cleanupClientOnboardingScenario+x}" ]
+then
+	cleanupClientOnboardingInternal=enableCleanupClientOnboarding=${cleanupClientOnboardingScenario}
+fi
+
+java -jar ${TOOLFILENAME} ${bootstrapDebugString} ${BOOTSTRAPURL} \"-scriptDownloadPath=${SCRIPTDOWNLOADPATH}\" \"-scriptName=${FILENAME}\" \"-scriptSource=${SCRIPTNAME}\" \"-scriptVersion=${SCRIPTVERSION}\" ${INTERNALOCLOGINSERVER} ${INTERNALOCLOGINTOKEN} ${INTERNALPAKINSTALLERPORTALURL} ${TOOLPROXYSETTINGS} -installBasePath=${DEPLOYMENTPATTERN} -config=${CONFIGNAME} -automationScript=${AUTOMATIONSCRIPT} ${cleanupClientOnboardingLabs_UserDataInternal} ${cleanupClientOnboardingLabsInternal} ${cleanupClientOnboardingInternal}
