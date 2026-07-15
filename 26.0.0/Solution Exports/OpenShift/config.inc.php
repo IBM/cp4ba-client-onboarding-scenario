a39cd0b0b3d2227271e89401f05bcb0e50c4ad30
OlafHahnl <Olaf.Hahnl@de.ibm.com>
2026-07-15 20:05:53 +0200

    Initial release for 26.0.0 Enterprise

---
+++ 26.0.0/Solution Exports/OpenShift/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+    $config['plugins'] = [];
+    $config['log_driver'] = 'stdout';
+    $config['zipdownload_selection'] = true;
+    $config['des_key'] = '7HPr7mIhN140t7/PgTPzwYG1';
+    $config['enable_spellcheck'] = true;
+    $config['spellcheck_engine'] = 'pspell';
+    include(__DIR__ . '/config.docker.inc.php');
+    
